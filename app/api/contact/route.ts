62e4ac26595368f3e4808dcc97fb21115ebb116d
zikran11 <ziikran123@gmail.com>
2026-05-30 21:29:29 +0700

    Update

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -18,7 +18,7 @@ export async function POST(req: Request) {
         const data = await resend.emails.send({
 from: 'Roota Contact <noreply@roota.nl>',
 
-            to: ['zikmazaya@gmail.com'],
+            to: ['ziikran123@gmail.com'],
 
             replyTo: email,
 
