c7b422f5ab7dfd1827deaae913539f14730510f7
zikran11 <ziikran123@gmail.com>
2026-05-30 20:49:49 +0700

    Update

--- app/api/application/route.ts
+++ app/api/application/route.ts
@@ -46,7 +46,7 @@ export async function POST(req: Request) {
         }
 
         await resend.emails.send({
-            from: 'Roota Careers <onboarding@resend.dev>',
+from: 'Roota Contact <noreply@roota.nl>',
 
             to: ['admin@roota.nl'],
 
--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -16,7 +16,7 @@ export async function POST(req: Request) {
         } = body
 
         const data = await resend.emails.send({
-            from: 'Roota Contact <onboarding@resend.dev>',
+from: 'Roota Contact <noreply@roota.nl>',
 
             to: ['info@roota.nl'],
 
